0b8fa4143088482750cba0d6351c0c752f9fe6a4
sirlexis <alex.agyapong@hotmail.com>
2018-04-02 10:34:45 +0100

    print statement of all transactions added

--- BankKata/BankKata.Domain/StatementPrinter.cs
+++ BankKata/BankKata.Domain/StatementPrinter.cs
@@ -14,7 +14,35 @@ namespace BankKata.Domain
 
         public void Print(List<Transaction> allTransactions)
         {
+            PrintStatementHeader();
+            PrintStatementLines(FormatStatementLines(allTransactions));
+        }
+
+        private void PrintStatementHeader()
+        {
             outputWriter.PrintLine(StatementHeader);
+        }
+
+        private void PrintStatementLines(IList<string> statementLines)
+        {
+            foreach (var line in statementLines)
+            {
+                outputWriter.PrintLine(line);
+            }
+        }
+
+        private IList<string> FormatStatementLines(IList<Transaction> transactions)
+        {
+            var statementLines = new List<string>();
+            var balance = 0;
+
+            foreach (var transaction in transactions)
+            {
+                balance += transaction.Amount;
+                statementLines.Add(($"{transaction.DateCreated} | {transaction.Amount:F} | {balance:F}"));
+            }
+            statementLines.Reverse();
+            return statementLines;
         }
     }
 }
--- BankKata/BankKata.Tests/StatementPrinterShould.cs
+++ BankKata/BankKata.Tests/StatementPrinterShould.cs
@@ -25,5 +25,28 @@ namespace BankKata.Tests
             statemenPrinter.Print(noTransactions);
             outputWriter.Verify(x => x.PrintLine(StatementHeader));
         }
+
+        [Test]
+        public void Print_statement_of_all_transactions()
+        {
+            var transactions = GetTransactions();
+            statemenPrinter.Print(transactions);
+
+            outputWriter.Verify(m => m.PrintLine("DATE | AMOUNT | BALANCE"));
+            outputWriter.Verify(m => m.PrintLine("10/04/2014 | 500.00 | 1400.00"));
+            outputWriter.Verify(m => m.PrintLine("02/04/2014 | -100.00 | 900.00"));
+            outputWriter.Verify(m => m.PrintLine("01/04/2014 | 1000.00 | 1000.00"));
+        }
+
+        private List<Transaction> GetTransactions()
+        {
+            var transactions = new List<Transaction>
+            {
+                new Transaction("01/04/2014",1000),
+                new Transaction("02/04/2014",-100),
+                new Transaction("10/04/2014",500)
+            };
+            return transactions;
+        }
     }
 }
